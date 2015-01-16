a7c44e50702251f660f8214c2da9ed6ab38b8a3a
d2tstudio <derry@d2tstudio.com>
2015-01-16 17:54:15 +0100

    Remove logo for now

--- package.js
+++ package.js
@@ -1,6 +1,7 @@
 Package.describe({
+  name: "d2tstudio:accounts-dribbble",
   summary: "Login service for Dribbble accounts",
-  version: "1.0.0",
+  version: "1.0.1",
   git: "https://github.com/d2tstudio/accounts-dribbble.git"
 });
 
