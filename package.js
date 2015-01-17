4c1d5fcf893e1f12a62332298a59e1b5e786a7d5
Derry Birkett <derry@d2tstudio.com>
2015-01-17 11:24:51 +0100

    Change login button to dribbble colour

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "d2tstudio:accounts-dribbble",
   summary: "Login service for Dribbble accounts",
-  version: "1.0.1",
+  version: "1.0.2",
   git: "https://github.com/d2tstudio/accounts-dribbble.git"
 });
 
@@ -10,7 +10,6 @@ Package.onUse(function(api) {
   // Export Accounts (etc) to packages using this one.
   api.imply('accounts-base@1.1.3', ['client', 'server']);
   api.use('accounts-oauth@1.1.3', ['client', 'server']);
-  api.use('github@1.1.2', ['client', 'server']);
 
   api.addFiles('dribbble_login_button.css', 'client');
 
